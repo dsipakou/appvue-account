3d8e5a4bb682f204edb32fee29fbcc3c3e868f16
Denis Sipakov <mc.flyer@gmail.com>
2021-07-13 11:34:58 +0300

    Add vuestic

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import { createApp } from 'vue';
-import App from './App.vue';
+import { VuesticPlugin } from 'vuestic-ui';
+import 'vuestic-ui/dist/vuestic-ui.css';
 import router from './router';
 import store from './store';
+import App from './App.vue';
 
-createApp(App).use(store).use(router).mount('#app');
+const app = createApp(App).use(store).use(router).mount('#app');
+app.use(VuesticPlugin);

e2b407c2d4176186ef7ce3bbbf76b010de74a1d7
Denis Sipakov <mc.flyer@gmail.com>
2021-07-14 16:32:06 +0300

    UI

--- src/views/Transactions.vue
+++ src/views/Transactions.vue
@@ -2,52 +2,70 @@
   <div>
     <h3>Transaction list</h3>
     <div class="drag-zone" id="transactionList">
-      <div class="drag-el" v-for="transaction in transactions" :key="transaction.id" draggable>
+      <div
+        class="drag-el"
+        v-for="transaction in transactions"
+        :key="transaction.id"
+        draggable="true"
+      >
         {{ transaction.category }} {{ transaction.amount }}
       </div>
     </div>
     <div id="transactionCreate">
-      <form>
-        <h3>Add transaction</h3>
-
-        <div>
-          <label>User</label>
-          <select v-model="input.user">
-            <option disabled value="">Select user</option>
-            <option
-              v-bind:value="user.id"
-              v-for="user in users"
-              :key="user.id">{{user.name}}
-            </option>
-          </select>
-        </div>
-        <div>
-          <label>Account</label>
-          <select v-model="input.account">
-            <option disabled value="">Select account</option>
-            <option
-              v-bind:value="account.id"
-              v-for="account in accounts"
-              :key="account.id">{{account.source}} {{account.id}}
-            </option>
-          </select>
-        </div>
-        <div>
-          <label>Category</label>
-          <input v-model="input.category" placeholder="Category" />
-        </div>
-        <div>
-          <label>Amount</label>
-          <input v-model="input.amount" placeholder="Amount" />
-        </div>
-        <div>
-          <label>Description</label>
-          <textarea v-model="input.description" placeholder="Description" />
-        </div>
-        <div>
-          <button type="button" v-on:click="create()">Create transaction</button>
-        </div>
-      </form>
+      <h3>Add transaction</h3>
+      <div class="va-table-responsive">
+        <form>
+          <table class="va-table">
+            <tr>
+              <td><label>User</label></td>
+              <td>
+                <select v-model="input.user">
+                  <option disabled value="">Select user</option>
+                  <option
+                    v-bind:value="user.id"
+                    v-for="user in users"
+                    :key="user.id">{{user.name}}
+                  </option>
+                </select>
+              </td>
+            </tr>
+            <tr>
+              <td><label>Account</label></td>
+              <td>
+                <select v-model="input.account">
+                  <option disabled value="">Select account</option>
+                  <option
+                    v-bind:value="account.id"
+                    v-for="account in accounts"
+                    :key="account.id">{{account.source}} {{account.id}}
+                  </option>
+                </select>
+              </td>
+            </tr>
+            <tr>
+              <td><label>Category</label></td>
+              <td>
+                <input v-model="input.category" placeholder="Category" />
+              </td>
+            </tr>
+            <tr>
+              <td><label>Amount</label></td>
+              <td>
+                <input v-model="input.amount" placeholder="Amount" />
+              </td>
+            </tr>
+            <tr>
+              <td><label>Description</label></td>
+              <td><textarea v-model="input.description" placeholder="Description" /></td>
+            </tr>
+            <tr>
+              <td colspan="2">
+                <button type="button" v-on:click="create()">Create transaction</button>
+              </td>
+            </tr>
+          </table>
+        </form>
+      </div>
     </div>
   </div>
 </template>
@@ -97,6 +115,10 @@ export default {
 };
 </script>
 <style scoped>
+#transactionCreate {
+  max-width: 1200px;
+  margin: auto;
+}
   .drag-el {
     background-color: #fff;
     margin-bottom: 10px;

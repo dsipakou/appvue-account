1ba58004772aa1d67cb6f42e5867980061011baa
Denis Sipakov <mc.flyer@gmail.com>
2021-08-07 12:15:02 +0300

    Get and store rates

--- src/views/Currencies.vue
+++ src/views/Currencies.vue
@@ -6,7 +6,20 @@
       </div>
     </div>
     <div class="row justify-center">
-      <q-btn rounded color="primary" @click="getCurrentRate">Get rates</q-btn>
+      <q-btn
+        rounded
+        color="primary"
+        @click="getCurrentRate"
+        :disabled="ratesInProgress">
+        Get rates
+      </q-btn>
+      <q-btn
+        rounded
+        color="primary"
+        class="q-ml-lg"
+        @click="createCurrency = true">
+        Add currency
+      </q-btn>
     </div>
     <div class="row justify-center q-mt-lg">
       <q-date v-model="days" multiple mask="YYYY-MM-DD"></q-date>
@@ -19,6 +32,19 @@
         :label="currency.verbalName"
         :val="currency"/>
     </div>
+    <q-dialog v-model="createCurrency">
+      <q-card class="shadow-24" style="width: 400px;">
+        <q-card-section>
+          <h4>Add currency</h4>
+        </q-card-section>
+
+        <q-separator />
+
+        <q-card-section>
+          <q-input outlined stack-label label="Code" v-model="input.code" />
+        </q-card-section>
+      </q-card>
+    </q-dialog>
   </div>
 </template>
 <script>
@@ -30,10 +56,20 @@ import { getRate } from '../service';
 export default {
   name: 'Currencies',
 
+  data() {
+    return {
+      ratesInProgress: false,
+      input: {
+        code: '',
+      },
+    };
+  },
+
   setup() {
     return {
       days: ref([]),
       selectedCurrencies: ref([]),
+      createCurrency: ref(false),
     };
   },
 
@@ -52,31 +88,28 @@ export default {
     ]),
 
     async getCurrentRate() {
+      this.ratesInProgress = true;
       await Promise.all(this.days.map(async (day) => {
         await Promise.all(this.selectedCurrencies.map(async (currency) => {
-          const rate = await getRate(currency.code, day);
-          const payload = {
-            currencyId: currency.id,
-            rateDate: day,
-            rate,
-            description: '',
-          };
+          const existingRate = this.ratesList.find((item) => {
+            const itemDate = moment(day).startOf('day');
+            const rateDate = moment(item.rateDate).startOf('day');
+            return itemDate.isSame(rateDate);
+          });
+          if (!existingRate) {
+            const rate = await getRate(currency.code, day);
+            const payload = {
+              currencyId: currency.id,
+              rateDate: day,
+              rate,
+              description: '',
+            };
 
-          await this.storeRate(payload);
+            this.createRate(payload);
+          }
         }));
       }));
-    },
-
-    async storeRate(rate) {
-      const existingRate = this.ratesList.find((item) => {
-        const itemDate = moment(item.rateDate).startOf('day');
-        const rateDate = moment(rate.rateDate).startOf('day');
-        return itemDate.isSame(rateDate);
-      });
-
-      if (!existingRate) {
-        this.createRate(rate);
-      }
+      this.ratesInProgress = false;
     },
   },
 };

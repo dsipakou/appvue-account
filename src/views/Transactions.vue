e7163cd28fe474fa49fb706f8792be4221143a13
Denis Sipakov <mc.flyer@gmail.com>
2021-08-16 12:40:21 +0300

    Store last date

--- src/views/Transactions.vue
+++ src/views/Transactions.vue
@@ -576,7 +576,7 @@ export default {
     onDrop(evt, category) {
       this.input.category = category.id;
       this.input.currency = this.defaultCurrency;
-      this.input.transactionDate = new Date().toISOString().substr(0, 10);
+      this.input.transactionDate ||= new Date().toISOString().substr(0, 10);
       this.input.amount = '';
       this.input.account = Number(evt.dataTransfer.getData('accountID'));
       this.input.user = Number(evt.dataTransfer.getData('userID'));

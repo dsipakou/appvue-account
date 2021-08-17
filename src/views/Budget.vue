844cd342b38563063a0f5e74468fb7c00cdb4b1f
Denis Sipakov <mc.flyer@gmail.com>
2021-08-17 13:30:19 +0300

    Add some budget layout

--- src/views/Budget.vue
+++ src/views/Budget.vue
@@ -1,22 +1,30 @@
 <template>
-  <div class="q-pa-md">
-    <div class="row justify-center">
-      <div class="header">
-        Budget
+  <div>
+    <div class="q-pa-md">
+      <div class="row q-mt-lg justify-center">
+        <div class="col-9 q-px-md">
+          <div class="row justify-between">
+            <h5>Monthly Budget</h5>
+            <q-btn round color="primary" label="+" @click="openAddForm"></q-btn>
+          </div>
+        </div>
+        <div class="col-3 q-px-md">
+          <div class="row justify-between">
+            <h5>This week</h5>
+            <span>'$1000'</span>
+          </div>
+        </div>
       </div>
     </div>
-    <div>
-      <q-btn round color="primary" label="+" @click="openAddForm"></q-btn>
-    </div>
+    <q-dialog v-model="createForm">
+      <q-card style="width: 400px;">
+        <div>Make your plans</div>
+        <div class="q-pa-md">
+          <q-input></q-input>
+        </div>
+      </q-card>
+    </q-dialog>
   </div>
-  <q-dialog v-model="createForm">
-    <q-card style="width: 400px;">
-      <div>Make your plans</div>
-      <div class="q-pa-md">
-        <q-input></q-input>
-      </div>
-    </q-card>
-  </q-dialog>
 </template>
 <script>
 import { ref } from 'vue';

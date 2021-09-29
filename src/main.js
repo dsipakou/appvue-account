900f34ddaba02fce2c2c9d3c72626f7d15c6a1cc
Denis Sipakov <mc.flyer@gmail.com>
2021-09-29 15:13:54 +0300

    Calendar config

--- src/main.js
+++ src/main.js
@@ -8,8 +8,36 @@ import App from './App.vue';
 import 'vuestic-ui/dist/vuestic-ui.css';
 import quasarUserOptions from './quasar-user-options';
 
-moment.locale('ru');
 moment.updateLocale('ru', {
+  calendar: {
+    sameDay: '[Сегодня]',
+    lastDay: '[Вчера]',
+    sameElse: 'LL',
+    lastWeek(now) {
+      if (now.week() !== this.week()) {
+        switch (this.day()) {
+          case 0:
+            return '[В прошлое] dddd';
+          case 1:
+          case 2:
+          case 4:
+            return '[В прошлый] dddd';
+          case 3:
+          case 5:
+          case 6:
+            return '[В прошлую] dddd';
+          default:
+            return 'dddd';
+        }
+      } else {
+        if (this.day() === 2) {
+          return '[Во] dddd';
+        }
+        return '[В] dddd';
+      }
+    },
+  },
+
   week: {
     dow: 1,
   },

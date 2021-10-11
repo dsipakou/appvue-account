56031becf391727d69a8c00a12ee17ff8aa1eec6
Denis Sipakov <mc.flyer@gmail.com>
2021-10-11 19:34:13 +0300

    Cont budget

--- src/views/budget/MonthPlanner.vue
+++ src/views/budget/MonthPlanner.vue
@@ -5,12 +5,21 @@
         <span class="text-h4">Monthly Planner</span>
       </div>
       <div class="row q-ma-sm">
-        <PlannerCard />
+        <PlannerCard
+          :items="budgetList.slice(0, 10)"
+        >
+        <template v-slot:title>
+          <span class="text-h5">
+            Week 1
+          </span>
+        </template>
+        </PlannerCard>
       </div>
     </div>
   </div>
 </template>
 <script>
+import { mapGetters } from 'vuex';
 import PlannerCard from './components/planner/PlannerCard.vue';
 
 export default {
@@ -18,6 +27,12 @@ export default {
 
   components: {
     PlannerCard,
+  },
+
+  computed: {
+    ...mapGetters([
+      'budgetList',
+    ]),
   },
 };
 </script>
--- src/views/budget/components/planner/PlannerCard.vue
+++ src/views/budget/components/planner/PlannerCard.vue
@@ -1,6 +1,22 @@
 <template>
-  <div>
-    Title
+  <div class="row">
+    <div class="vertical-middle">
+      <slot name="title"></slot>
+      <q-card class="card">
+        <q-card-section
+          v-for="item in items"
+          :key="item.id">
+          <q-card flat bordered
+            dragable="true"
+            @dragstart="$event">
+            <q-card-section dense>
+              {{ item.title }}
+            </q-card-section>
+          </q-card>
+        </q-card-section>
+        <q-separator />
+      </q-card>
+    </div>
   </div>
   <div>
     body
@@ -12,5 +28,15 @@
 <script>
 export default {
   name: 'PlannerCard',
+
+  props: {
+    items: { type: Array, required: true },
+  },
 };
 </script>
+<style scoped>
+.card {
+  min-width: 200px;
+  min-height: 400px;
+}
+</style>

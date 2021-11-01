8910fc0334b2db40c007de3fd43f563e145001dd
Denis Sipakov <mc.flyer@gmail.com>
2021-11-01 19:48:37 +0300

    Up:

--- src/views/currency/components/CurrencyChart.vue
+++ src/views/currency/components/CurrencyChart.vue
@@ -1,5 +1,6 @@
 <script>
 import { Line } from 'vue3-chart-v2';
+import { format, parse } from 'date-fns';
 
 export default {
   name: 'CurrencyChart',
@@ -14,7 +15,7 @@ export default {
   data: () => ({
     chartdata: {
       labels: [],
-      datasets: [],
+      datasets: [{ label: 'hello', data: [1, 2, 3] }],
     },
     options: {
       responsive: true,
@@ -22,20 +23,47 @@ export default {
     },
   }),
 
-  mounted() {
-    const currencyId = this.currencyList.find((item) => item.code === 'USD')?.id;
-    const currList = this.ratesList.filter((item) => item.currencyId === currencyId);
-    this.chartdata.labels.push(
-      this.currList?.map((item) => item.rateDate),
-    );
-    this.chartdata.datasets.push({
-      label: 'USD',
-      data: currList.map((item) => item.rate)?.slice(0, 20),
-    });
-    console.log(currList);
-    console.log(this.currencyList.find((item) => item.code === 'USD'));
+  methods: {
+    updateChart() {
+      const currencyId = this.currencyList.find((item) => item.code === 'USD')?.id;
+      const eurId = this.currencyList.find((item) => item.code === 'EUR')?.id;
+      const currList = this.ratesList.filter((item) => item.currencyId === currencyId);
+      const eurList = this.ratesList.filter((item) => item.currencyId === eurId);
+      const labels = currList?.map((item) => {
+        const parsed = parse(item.rateDate, 'yyyy-MM-dd\'T\'HH:mm:ssX', new Date());
+        const formatted = format(parsed, 'MMM-dd');
+        return formatted;
+      })?.slice(0, 30).reverse();
+      const dataList = currList.map((item) => item.rate)?.slice(0, 30).reverse();
+      const eurDataList = eurList.map((item) => item.rate)?.slice(0, 30).reverse();
+      const dataSetData = {
+        label: 'USD',
+        backgroundColor: '#f87979',
+        borderColor: '#153645',
+        data: dataList,
+      };
+      const eurSetData = {
+        label: 'EUR',
+        data: eurDataList,
+      };
+      this.chartdata = {
+        labels,
+        datasets: [dataSetData, eurSetData],
+      };
+    },
+  },
 
+  mounted() {
     this.renderChart(this.chartdata, this.options);
+  },
+
+  watch: {
+    ratesList() {
+      if (this.currencyList.length > 0 && this.ratesList.length > 0) {
+        this.updateChart();
+        this.renderChart(this.chartdata, this.options);
+      }
+    },
   },
 };
 </script>

826ac2dc2440b0bdeacad0b736174530db66314e
Denis Sipakov <mc.flyer@gmail.com>
2021-11-03 21:13:00 +0300

    Add currency interface

---
+++ src/types/Currency.ts
@@ -0,0 +1,10 @@
+interface Currency {
+  id: nubmer,
+  code: string,
+  sign: string,
+  verbalName: string,
+  isDefault: boolean,
+  comments: string,
+  createdAt: string,
+  updatedAt: string,
+}

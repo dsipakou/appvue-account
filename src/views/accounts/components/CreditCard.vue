cca41c7b5e14fc26c980900d8d6f842af78f8797
Denis Sipakov <mc.flyer@gmail.com>
2022-01-02 19:51:49 +0100

    Rework account cont

--- src/views/accounts/components/CreditCard.vue
+++ src/views/accounts/components/CreditCard.vue
@@ -1,5 +1,9 @@
 <template>
-  <div>Hello</div>
+  <div class="card card--front">
+    <div class="card__number">4111 1111 1111 1111</div>
+    <div class="card__expiry-date">10/17</div>
+    <div class="card__owner">Jane Doe</div>
+  </div>
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue';
@@ -8,3 +12,19 @@ export default defineComponent({
   name: 'CreditCard',
 });
 </script>
+<style scoped>
+.card {
+  font: 16px/1.5 'Helvetica Neue', Helvetica, sans-serif;
+  position: relative;
+  display: inline-block;
+  vertical-align: middle;
+  width: 375px;
+  height: 220px;
+  text-align: left;
+  padding: 30px;
+  margin-bottom: 50px;
+  color: #fff;
+  border-radius: 20px;
+  box-sizing: border-box;
+}
+</style>

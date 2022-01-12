dae388057b4088a714cd10ec175a117f8445445e
Denis Sipakov <mc.flyer@gmail.com>
2022-01-12 19:33:31 +0100

    Update login page

--- src/views/user/Login.vue
+++ src/views/user/Login.vue
@@ -1,14 +1,14 @@
 <template>
-  <q-page class="bg-light-blue window-height row justify-center items-center">
+  <q-page class="bg-white window-height row justify-center items-center">
     <div class="column">
       <div class="row">
-        <h5 class="text-h5 text-white q-my-md">Company & Co</h5>
+        <h5 class="text-h5 text-black q-my-md">Login to Flying Budget</h5>
       </div>
       <div class="row">
         <q-card square bordered class="q-pa-lg shadow-1">
           <q-card-section>
             <q-form class="q-gutter-md">
-              <q-input square filled clearable v-model="email" type="email" label="email" />
+              <q-input square filled v-model="email" type="email" label="email" />
               <q-input square filled clearable
                 v-model="password"
                 type="password"
@@ -16,7 +16,7 @@
             </q-form>
           </q-card-section>
           <q-card-actions class="q-px-md">
-            <q-btn unelevated color="light-green-7" size="lg" class="full-width" label="Login" />
+            <q-btn unelevated no-caps color="primary" size="lg" class="full-width" label="Login" />
           </q-card-actions>
           <q-card-section class="text-center q-pa-none">
             <p class="text-grey-6">Not reigistered? Created an Account</p>
@@ -31,5 +31,17 @@ import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'Login',
+
+  data() {
+    return {
+      email: '',
+      password: '',
+    };
+  },
 });
 </script>
+<style scoped>
+.q-card {
+  width: 460px;
+}
+</style>

848acda08fb398e8d0a0512193382c4c33407f27
Denis Sipakov <mc.flyer@gmail.com>
2022-02-13 16:01:37 +0100

    Get months from date-fns

--- src/components/dropdown/MonthDropdown.vue
+++ src/components/dropdown/MonthDropdown.vue
@@ -2,19 +2,24 @@
   <q-select stack-label dense
     label="Month"
     class="month-dropdown"
-    :options="items"
+    :options="monthsList"
     v-model="dropdown"/>
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue';
+import { enUS } from 'date-fns/locale';
 
 export default defineComponent({
   name: 'MonthDropdown',
 
-  setup() {
-    return {
-      items: ['Jan', 'Feb', 'Mar', 'Arp', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-    };
+  computed: {
+    monthsList() {
+      const months = [];
+      for (let i = 0; i < 12; i += 1) {
+        months.push(enUS.localize.month(i, { width: 'abbreviated' }));
+      }
+      return months;
+    },
   },
 });
 </script>

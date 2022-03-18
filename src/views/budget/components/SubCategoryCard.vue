8151845bef69f05659420b7edf28e1470a0cadb8
Denis Sipakov <mc.flyer@gmail.com>
2022-03-18 21:16:59 +0100

    Sub card skeleton

--- src/views/budget/components/SubCategoryCard.vue
+++ src/views/budget/components/SubCategoryCard.vue
@@ -1,6 +1,27 @@
 <template>
   <q-card flat class="container">
-    <div class="row title--main">{{ item.name }}</div>
+    <div class="row col-12 title--main">{{ item.name }}</div>
+    <div class="row progress-container">
+      <div class="row col-12 justify-center remains">
+        <span>53 left</span>
+      </div>
+      <div class="row desc-text multiply">
+        <span>2x</span>
+      </div>
+      <q-linear-progress
+        class="progress-bar"
+        value="0.46"
+        color="green-14"
+        size="34px"
+        track-color="grey-6">
+      </q-linear-progress>
+      <div class="row bottom">
+        <div class="row desc-text col-6">
+          <span>250 spent</span></div>
+        <div class="row desc-text justify-end col-6">
+          <span>of 300</span></div>
+      </div>
+    </div>
   </q-card>
 </template>
 <script lang="ts">
@@ -49,4 +70,34 @@ export default defineComponent({
   white-space: nowrap;
   font-size: 16px;
 }
+
+.remains {
+  margin-top: 5px;
+  font-size: 14px;
+}
+
+.progress-container {
+  margin: 0 36px;
+}
+
+.multiply {
+  font-size: 10px;
+  margin-bottom: 5px;
+}
+
+.progress-bar {
+  font-size: 13px;
+  font-weight: bold;
+  color: white;
+}
+
+.bottom {
+  font-size: 10px;
+  margin-top: 5px;
+}
+
+.desc-text {
+  padding: 0 5px;
+}
+
 </style>

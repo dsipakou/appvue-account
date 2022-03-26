e7ea1e2db4ddcb6de89b3d66dbcf6f4e7b4d0635
Denis Sipakov <mc.flyer@gmail.com>
2022-03-26 15:08:44 +0100

    Move grouping into utils

--- src/utils/budgetUtils.ts
+++ src/utils/budgetUtils.ts
@@ -1,5 +1,5 @@
 /* eslint max-len: ["error", { "code": 120, "ignoreComments": true }] */
-/* eslint class-methods-use-this: ["error", { "exceptMethods": ["groupedByName", "sortByDate", "countPlannedBudget", "groupedByCategory"]}] */
+/* eslint class-methods-use-this: ["error", { "exceptMethods": ["groupedByName", "sortByField", "countPlannedBudget", "groupedByCategory"]}] */
 
 import { BudgetUsage } from '@/types/Budget';
 import { Category } from '@/types/Category';
@@ -8,19 +8,30 @@ interface GroupedByName {
   [key: string]: BudgetUsage[],
 }
 
+interface GroupedByCategoryItem {
+  name: string,
+  items: BudgetUsage[],
+  planned: number,
+  actualUsage: number,
+}
+
+interface GroupedByCategory {
+  [key: string]: GroupedByCategoryItem[],
+}
+
 interface CountPlannedBudget {
   ids: number[],
   sum: number,
 }
 
 class BudgetUtils {
-  private sortByDate(items: BudgetUsage[]): BudgetUsage[] {
+  private sortByField(items: any, field: string): any[] {
     return items.sort(
-      (a: { budgetDate: string }, b: { budgetDate: string }) => {
-        if (a.budgetDate < b.budgetDate) {
+      (a: any, b: any) => {
+        if (a[field] < b[field]) {
           return -1;
         }
-        if (a.budgetDate > b.budgetDate) {
+        if (a[field] > b[field]) {
           return 1;
         }
         return 0;
@@ -53,22 +64,22 @@ class BudgetUtils {
     budgetList: BudgetUsage[],
     categoryItems: Category[],
   ): object {
-    const categoryClass: {[key: string]: any[]} = {};
-    Object.entries(this.groupedByName(budgetList)).forEach((item) => {
-      const { categoryId } = item[1][0];
+    const categoryClass: GroupedByCategory = {};
+    Object.entries(this.groupedByName(budgetList)).forEach(([key, value]) => {
+      const { categoryId } = value[0];
       const categoryName = categoryId === null
         ? 'undefined'
         : categoryItems.find((category: Category) => (
           category.id === categoryId
         ))!.name;
-      const arr: any[] = categoryClass[categoryName] || [];
-      const sortedGroupedBudgets = this.sortByDate(item[1]);
-      const countedPlannedObject = this.countPlannedBudget(item[1]);
-      const group = {
-        name: item[0],
+      const arr: GroupedByCategoryItem[] = categoryClass[categoryName] || [];
+      const sortedGroupedBudgets: BudgetUsage[] = this.sortByField(value, 'budgetName');
+      const countedPlannedObject: CountPlannedBudget = this.countPlannedBudget(value);
+      const group: GroupedByCategoryItem = {
+        name: key,
         items: sortedGroupedBudgets,
         planned: countedPlannedObject.sum,
-        actualUsage: item[1].reduce(
+        actualUsage: value.reduce(
           (
             acc: number,
             subItem: { spentInBaseCurrency: number },
@@ -81,18 +92,20 @@ class BudgetUtils {
     return categoryClass;
   }
 
-  groupedBudgetUsage(budgetList: BudgetUsage[], categoryItems: Category[]) {
-    const groupedList: any[] = [];
-    Object.entries(this.groupedByCategory(budgetList, categoryItems)).forEach((item) => {
-      const categoryItem = {
-        name: item[0],
-        items: item[1],
-        planned: item[1].reduce(
-          (acc: number, subItem: { planned: number }) => acc + subItem.planned, 0,
-        ),
-        actualUsage: item[1].reduce(
-          (acc: number, subItem: { actualUsage: number }) => acc + subItem.actualUsage, 0,
-        ),
+  groupedBudgetUsage(budgetList: BudgetUsage[], categoryItems: Category[]): GroupedByCategoryItem[] {
+    const groupedList: GroupedByCategoryItem[] = [];
+    Object.entries(this.groupedByCategory(budgetList, categoryItems)).forEach(([key, value]) => {
+      const planned = value.reduce(
+        (acc: number, subItem: GroupedByCategoryItem) => acc + subItem.planned, 0,
+      );
+      const actualUsage = value.reduce(
+        (acc: number, subItem: GroupedByCategoryItem) => acc + subItem.actualUsage, 0,
+      );
+      const categoryItem: GroupedByCategoryItem = {
+        name: key,
+        items: value,
+        planned,
+        actualUsage,
       };
       if (categoryItem.name === 'undefined') {
         groupedList.unshift(categoryItem);
@@ -100,11 +113,8 @@ class BudgetUtils {
         groupedList.push(categoryItem);
       }
     });
-    return groupedList.sort((a: { name: string }, b: { name: string }) => {
-      if (a.name < b.name) return -1;
-      if (a.name > b.name) return 1;
-      return 0;
-    });
+
+    return this.sortByField(groupedList, 'name');
   }
 }
 
